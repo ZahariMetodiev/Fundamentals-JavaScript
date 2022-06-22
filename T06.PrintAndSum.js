c57cf0bd2f50d6d5ea7d195ee3ae95d3f852df0f
ZahariMetodiev <zmetodiev4@gmail.com>
2022-06-22 22:22:11 +0300

    Add task PrintAndSum

---
+++ T06.PrintAndSum.js
@@ -0,0 +1,10 @@
+function printAndSum(v1, v2) {
+    let sum = 0
+    let numbers = []
+    for (let i = v1; i <= v2; i++) {
+        numbers.push(i)
+        sum += i
+    }
+    return `${numbers.join(' ')}
+Sum: ${sum}`
+}
