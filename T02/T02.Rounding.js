64d4d73a3efdb2a04f1a6db3eefd6a27b7ef6344
ZahariMetodiev <zmetodiev4@gmail.com>
2022-06-24 11:15:22 +0300

    Rename folder

--- T02/T02.Rounding.js
+++
@@ -1,3 +0,0 @@
-function rounding(x, y) {
-    return parseFloat(x.toFixed(y < 15 ? y : 15))
-}
--- T02/T05.LeapYear.js
+++
@@ -1,3 +0,0 @@
-function leapYear (y) {
-    return ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0) ? 'yes' : 'no' 
-}
